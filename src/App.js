1ec0a6b0dd9b4c5cb7f89d5cad7be0be5c312e8f
ibshaya <s202176470@kfupm.edu.sa>
2026-04-08 23:01:00 +0300

    feat: Restructure Dashboard with As Renter and As Lender tabs, add review form fields

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import SignIn from './pages/SignIn';
 import Marketplace from './pages/Marketplace';
 import EquipmentDetail from './pages/EquipmentDetail';
 import Checkout from './pages/Checkout';
-import RenterDashboard from './pages/RenterDashboard';
+import Dashboard from './pages/Dashboard';
 import './index.css';
 
 function App() {
@@ -26,7 +26,7 @@ function App() {
               <Route path="/marketplace" element={<Marketplace />} />
               <Route path="/equipment/:id" element={<EquipmentDetail />} />
               <Route path="/checkout/:id" element={<Checkout />} />
-              <Route path="/renter-dashboard" element={<RenterDashboard />} />
+              <Route path="/dashboard" element={<Dashboard />} />
               {/* Other routes will be added here */}
             </Routes>
           </main>
